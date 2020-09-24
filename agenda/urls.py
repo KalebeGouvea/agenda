0aacac97d06c88f47b0b9e7e36a7481a2cff9c30
Kalebe Gouvea <k.gouvea@live.com>
2020-09-24 12:15:09 -0300

    Add update e delete de dados

--- agenda/urls.py
+++ agenda/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('agenda/', views.lista_eventos),
     path('agenda/evento/', views.evento),
     path('agenda/evento/submit', views.submit_evento),
+    path('agenda/evento/delete/<id_evento>', views.delete_evento),
     path('', views.index),
     path('login/', views.login_user),
     path('login/submit', views.submit_login),
--- core/models.py
+++ core/models.py
@@ -19,3 +19,6 @@ class Evento(models.Model):
     
     def get_data_evento(self):
         return self.data_evento.strftime('%d/%m/%Y às %H:%M')
+    
+    def get_data_input_evento(self):
+        return self.data_evento.strftime('%Y-%m-%dT%H:%M')
